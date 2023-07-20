e5ba8ec9f4da5461925f1cf6accb0c2b89a5dcb2
Zayco <martinhernan1996@gmail.com>
2023-07-20 13:27:20 -0300

    Integracion de Async-Await a la App realizada

--- Client/src/App.js
+++ Client/src/App.js
@@ -10,8 +10,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 
-const email = 'martinhernan1996@gmail.com';
-const password = 'zzz666';
+const URL = 'http://localhost:3001/rickandmorty/login/';
 
 function App() {
   const location = useLocation();
@@ -19,15 +18,17 @@ function App() {
   const [characters, setCharacters] = useState([]);
   const [access, setAccess] = useState(false);
 
-  const login = (userData) => {
-    const { email, password } = userData;
-    const URL = 'http://localhost:3001/rickandmorty/login/';
-    axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
-       const { access } = data;
-       setAccess(access);
-       access && navigate('/home');
-    });
+  const login = async (userData) => {
+    try {
+      const { email, password } = userData;
+      const { data} = await axios(URL + `?email=${email}&password=${password}`) 
+      const { access } = data;
+      setAccess(access);
+      access && navigate('/home');
+  } catch (error) {
+      console.log(error.message);
  }
+}
 
   const logout = () => {
     setAccess(false);
@@ -36,24 +37,18 @@ function App() {
 
   useEffect(() => {
     !access && navigate('/');
- }, [access]);
+ }, [access, navigate]);
 
-  const onSearch = (id) => {
-    axios(`http://localhost:3001/rickandmorty/character/${id}`)
-      .then((response) => response.data)
-      .then((data) => {
+  const onSearch = async (id) => {
+    try {
+      const { data } = await axios(`http://localhost:3001/rickandmorty/character/${id}`);
         if (data.name) {
-          const characterExists = characters.some((char) => char.id === data.id);
-          if (!characterExists) {
             setCharacters((oldChars) => [...oldChars, data]);
-          } else {
-            window.alert('¡Este personaje ya se encuentra en la lista!');
-          }
-        } else {
-          window.alert('¡No hay personajes con este ID!');
-        }
-      });
-  };
+        };
+  } catch (error) {
+      window.alert('¡No hay personajes con este ID!');
+  }
+}
 
   const addRandomCharacter = () => {
     const randomId = Math.floor(Math.random() * 826);
@@ -75,7 +70,7 @@ function App() {
 
   const onClose = (id) => {
     const charactersFiltered = characters.filter(character =>
-      character.id !== Number(id))
+      character.id !== id)
     setCharacters(charactersFiltered)
    }
 
