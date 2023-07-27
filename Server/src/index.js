71676b50876982690d646e7348d71818a6b7a8fd
Zayco <martinhernan1996@gmail.com>
2023-07-27 16:40:21 -0300

    Integracion de Sequelize I a la App realizada

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,8 @@
 const PORT = 3001;
 const server = require('./app');
+const { conn } = require('./DB_connection');
 
-server.listen(PORT, () => {
+conn.sync({ force: true }).then(() => {
+   server.listen(PORT, () => {
    console.log(`Server raised in port:${PORT}`);
-});
+})});
